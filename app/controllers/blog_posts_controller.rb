ea9e90880cd044c6857eaaf53a9b3b332e9bdab3
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-05-30 15:01:28 +0300

    Create links from index to a blog post page
    Add link_to helper on index.html.erb

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,7 +5,7 @@ class BlogPostsController < ApplicationController
 
   def show
     @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
+  rescue ActiveRecord::RecordNotFound => e
     redirect_to root_path
   end
 end
