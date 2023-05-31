0dbd6677bbbafb52985fa7f664d648d24b9d9435
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-05-31 09:44:32 +0300

    Create new.html.erb template
    Create a title, body fields and a submit button using form_with helper

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -10,6 +10,10 @@ class BlogPostsController < ApplicationController
   end
 
   def new
-
+    # Create a new blog post in a memory but not save it to the database
+    # It will just be completely empty.
+    # And we can use this to give to Rails to generate a form.
+    # And Rails will take the form fields and match them to the correct database columns for us.
+    @blog_post = BlogPost.new
   end
 end
