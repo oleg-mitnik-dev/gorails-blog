8c8daae6824280c1fa01a3989d597c4f12974d1a
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-05-31 09:20:34 +0300

    Create route to a #new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # Rails routes are matched in the order they are specified
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_pos
+
   root "blog_posts#index"
 end
