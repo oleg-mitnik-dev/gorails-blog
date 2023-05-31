3909cbb54d281b01bfef984adad439c3690d87ff
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-05-31 08:37:15 +0300

    Rails routes order
    Example of incorrect routes order

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_pos
   root "blog_posts#index"
 end
