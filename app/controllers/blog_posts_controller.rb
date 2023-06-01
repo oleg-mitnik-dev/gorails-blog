bfd20851e8286e9cd8a856cb2d2ffdea43720969
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-06-01 10:52:16 +0300

    Create #update action
    Create #update action and PATCH route for it.
    Create @blog_post.update error handling.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -32,7 +32,17 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
+    # ID from the URL contains in our params[:id]
+    @blog_post = BlogPost.find(params[:id])
+  end
 
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # Rails routes are matched in the order they are specified
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+
+  # Rails path helpers does not care about request type
+  # So we can have :blog_post name only on a single one request
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
+
   get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
