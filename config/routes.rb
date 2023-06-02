7b2858c467b122b3a4c256086d26eeab077ed5b3
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-06-02 10:28:34 +0300

    Add button Profile if user signed in
    Run migrations, and add a link_to a user Profile registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # This is a special route that allows all of those devise
+  # sign-up links and login links and edit-your-profile links
   devise_for :users
+
   resources :blog_posts
 
   root "blog_posts#index"
