de0694b2f7627ea6c67ba9e9703512fc0b67d449
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-06-02 15:48:15 +0300

    Find or create a user if it does not exist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# The way we wrote it here instead of using "> User.create" is going to allow us to rerun this without errors.
+# So if we run it first time will not find the user in the database, and it will create one with this password and email address.
+# If we run it again it's going to look up the record in the database, and then update it with the new password.
+# So we can un it many times and it's not going to throw any errors. Which is nice!
+# So we want our seeds to be repeatable like that.
+
+# We will look up a user in the database with that email
+# or will create a new one in memory: initiallize a new one in memory
+user = User.where(email: "examle@gmail.com").first_or_initialize
+
+# And we can update the user with a new password
+# update! thow an exception instead of just returning false
+user.update!(
+  password: "password",
+  password_confirmation: "password"
+)
