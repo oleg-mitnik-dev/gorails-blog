299a87dcb37fd5766afedd602efc8d84f09bae8f
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-06-09 15:57:35 +0300

    Use fixtures for our test

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -4,42 +4,44 @@ class BlogPostTest < ActiveSupport::TestCase
   # draft
 
   test "draft returns true for draft blog post" do
-    assert BlogPost.new(published_at: nil).draft?
+    # it is a little slower because it is going to query the database for a fixture
+    # but it also is really handy because we can use this for other tests in our code as well
+    assert blog_posts(:draft).draft?
   end
 
   test "draft returns false for published blog post" do
-    refute BlogPost.new(published_at: 1.year.ago).draft?
+    refute blog_posts(:published).draft?
   end
 
   test "draft returns false for scheduled blog post" do
-    refute BlogPost.new(published_at: 1.year.from_now).draft?
+    refute blog_posts(:scheduled).draft?
   end
 
   # published
 
   test "published returns true for published blog post" do
-    assert BlogPost.new(published_at: 1.year.ago).published?
+    assert blog_posts(:published).published?
   end
 
   test "published returns false for draft blog post" do
-    refute BlogPost.new(published_at: nil).published?
+    refute blog_posts(:draft).published?
   end
 
   test "published returns false for scheduled blog post" do
-    refute BlogPost.new(published_at: 1.year.from_now).published?
+    refute blog_posts(:scheduled).published?
   end
 
   # scheduled
 
   test "scheduled returns true for scheduled blog post" do
-    assert BlogPost.new(published_at: 1.year.from_now).scheduled?
+    assert blog_posts(:scheduled)q.scheduled?
   end
 
   test "scheduled returns false for draft blog post" do
-    refute BlogPost.new(published_at: nil).scheduled?
+    refute blog_posts(:draft).scheduled?
   end
 
   test "scheduled returns false for published blog post" do
-    refute BlogPost.new(published_at: 1.year.ago).scheduled?
+    refute blog_posts(:published).scheduled?
   end
 end
