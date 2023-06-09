2d61b7c611b3658c489f3249f7e2a49f90e6557a
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-06-09 09:28:53 +0300

    Create datetime field for each post for scheduling

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,4 +3,12 @@ class BlogPost < ApplicationRecord
   # Otherwise call error when we try to call @blog_post.save
   validates :title, presence: true
   validates :body, presence: true
+
+  # The lambda allows us to evaluate this every time we make a query for
+  # BlogPost.scheduled,
+  # BlogPost.published,
+  # BlogPost.draft
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
 end
