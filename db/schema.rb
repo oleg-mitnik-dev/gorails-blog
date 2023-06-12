ce53533ca61963e90cb2beda8f6d07da791d589f
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-06-12 15:34:40 +0300

    Add rich text editor.
    Add has_rich_text :content to our BlogPost model.
    Replace :body to :content in show and _form templates.
    Add :content to our permitted variables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_064932) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_072714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_064932) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "published_at"
